e874a85cc809dd9b607c8c38ecc4af7bba59b9d1
Tom <thomaskambale1@gmail.com>
2024-05-28 19:50:25 +0300

    header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import {
+  AppBar,
+  CssBaseline,
+  Slide,
+  Toolbar,
+  Typography,
+  useScrollTrigger,
+} from "@mui/material";
 
-function Header() {
-  return (
-    <div>Celestial design Header</div>
-  )
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: React.ReactElement;
 }
 
-export default Header
+function HideOnScroll(props: Props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+function Header(props: Props) {
+  return (
+    <div>
+      Celestial design Header
+      <React.Fragment>
+        <CssBaseline />
+        <HideOnScroll {...props}>
+          <AppBar>
+            <Toolbar>
+              <Typography variant="h6" component="div">
+                Scroll to hide App bar
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </HideOnScroll>
+        <Toolbar />
+      </React.Fragment>
+    </div>
+  );
+}
+
+export default Header;
