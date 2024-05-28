d9da1c29b4dd70f8212bb4c9bf30d0b90175e577
Tom <thomaskambale1@gmail.com>
2024-05-28 19:31:04 +0300

    First commit, file structure prepared, app.js structures, material UI and icons installed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { Route, Router } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import FAQ from './pages/FAQ';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Route path='/' Component={Home}/>
+      <Route path='' Component={About}/>
+      <Route path='' Component={Services}/>
+      <Route path='' Component={Projects}/>
+      <Route path='' Component={Contact}/>
+      <Route path='' Component={FAQ}/>
+      {/* <Route path='' Component={About}/>
+      <Route path='' Component={About}/> */}
+
+
+
+
+
+
+      <Footer/>
+    </Router>
   );
 }
 
